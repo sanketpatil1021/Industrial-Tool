2fdbbf22e87e51f1a67a37607052add1121558eb
sanketpatil1021 <sanketpatil3282@gmail.com>
2024-07-31 12:20:03 +0530

    addition of two numbers

---
+++ addition.java
@@ -0,0 +1,13 @@
+public class addition{
+public static void main(String args[])
+{
+
+int a=50;
+int b=90;
+
+System.out.println("First No is:"+a);
+System.out.println("Second No is:"+b);
+System.out.println("Addition is:"+(a+b));
+
+}
+}
